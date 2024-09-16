ce1be891c44e3241e61d9ab2e0a565aa9d93ac23
Duoduo Xu <56886703+xud0218@users.noreply.github.com>
2024-09-15 21:47:11 -0400

    Create add_two.py

---
+++ add_two.py
@@ -0,0 +1,9 @@
+def add_two_numbers(num1, num2):
+    return num1 + num2
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+sum_result = add_two_numbers(number1, number2)
+
+print(f"The sum of {number1} and {number2} is {sum_result}")
